678396356cae5635fe3e16da3a4a5493271e74ae
Adeola Fadare <adeolafadare8@gmail.com>
2022-07-20 10:50:23 +0100

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,21 @@ $(document).ready(function() {
         $('#hamburger').toggleClass('open')
     })
 });
-
-
-
-
+var obj = $('#support-team div:first')
+var obj2 = obj.siblings()
+$(window).scroll(function() {
+    var objB = obj.get(0).getBoundingClientRect();
+    var objB2 = obj2.get(0).getBoundingClientRect();
+    if (objB2.top <= 80+ obj.innerHeight() && objB2.bottom >= 80+ obj.innerHeight()) {
+        obj.css({'position': 'fixed', 'top': '80px', 'left': '0'})
+        obj2.css({'margin-top': `${obj.innerHeight()}px`})
+    }
+    else if(objB2.top > 80+ obj.innerHeight()) {
+        obj.css({'position': 'static'})
+        obj2.css({'margin-top': '0'})
+    }
+    else if(objB2.bottom < 80+ obj.innerHeight()) {
+        obj.css({'position': 'relative', 'top': `${obj2.innerHeight()}px`})
+        obj2.css({'margin-top': '0'})
+    }
+});
